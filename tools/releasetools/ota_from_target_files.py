7960889851a95e840335136c76d0365b5d987a33
Subinsmani <subins.mani@gmail.com>
2019-10-04 20:17:39 +0530

    Build: Add Pixys Edition to Rom Information
    Change-Id: Ia7c7ae4585aff667789e205bef80eada16b292c4
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -918,6 +918,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   build_id = target_info.GetBuildProp("ro.build.id")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
   device = target_info.GetBuildProp("ro.pixys.device")
+  edition = target_info.GetBuildProp("ro.pixys.edition")
 
   script.Print("--------------------------------------------");
   script.Print("  ___  ___  _   _ _    _  ____   ___   ___  ");
@@ -931,6 +932,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print(" Build id: %s"%(build_id));
   script.Print(" Security patch: %s"%(security_patch));
   script.Print(" Device: %s"%(device));
+  script.Print(" PixysOS Edition: %s"%(edition));
   script.Print("--------------------------------------------");
 
   device_specific.FullOTA_InstallBegin()
